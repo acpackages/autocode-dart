e9ec435fb7aa3ba545fa6e6fe60faf52c440d283
Sanket Patel <shreeharitechnologies2018@gmail.com>
2026-05-09 11:55:23 +0530

    Remove deleted test scripts

--- tests/autocode-tests/lib/autocode_tests.dart
+++ tests/autocode-tests/lib/autocode_tests.dart
@@ -1,7 +1,5 @@
 export 'sql/test_schema_manager.dart';
 export 'web/web_on_jaguar/test_web_on_jaguar.dart';
-import 'package:autocode_tests/web-sockets/demo_client.dart';
-import 'package:autocode_tests/web-sockets/demo_server.dart';
 import 'package:autocode_tests/web/web_on_jaguar/test_web_on_jaguar.dart';
 import 'ai_clients/providers/gemini.dart';
 import 'ai_clients/providers/openai.dart';
@@ -11,8 +9,8 @@ import 'sql/test_schema_manager.dart';
 void main() {
   // testSchemaManager();
   // testWebOnJaguarAutoApi();
-  testAcWebSocketServer();
-  testAcWebSocketClient();
+  // testAcWebSocketServer();
+  // testAcWebSocketClient();
   // testAimlApi();
   // testOpenAI();
   // testGemini();
